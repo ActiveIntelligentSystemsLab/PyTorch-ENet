c0e73ce980cd8c8a10b14001c4cb3c8ebd6127f0
Kazuki Mano <mano@aisl.cs.tut.ac.jp>
2019-10-03 21:09:01 +0900

    [add] Add a script to convert checkpoint file to .pt file

--- convert-to-pt.py
+++ convert-to-pt.py
@@ -2,8 +2,11 @@ import torch
 import torchvision
 import utils
 import torchvision.transforms as transforms
+import transforms as ext_transforms
 from models.enet import ENet
 from PIL import Image
+import torch.utils.data as data
+from data import CamVid as dataset
 
 num_classes = 12
 device = 'cuda'
@@ -11,7 +14,7 @@ model_path = 'save/ENet_CamVid/ENet'
 w = 480
 h = 264
 dataset_dir = '/tmp/dataset/CamVid'
-batch_size = 8
+batch_size = 1
 workers = 4
 
 # Image transform
@@ -36,6 +39,10 @@ val_loader = data.DataLoader(
     shuffle=False,
     num_workers=workers)
 
+# Intialize ENet
+model = ENet(num_classes).to(device)
+print("ENet initialized")
+
 # Create a batch
 for i, batch in enumerate(val_loader):
     # Get the inputs and labels
@@ -43,11 +50,18 @@ for i, batch in enumerate(val_loader):
 
     break
 
-# Intialize ENet
-model = ENet(num_classes).to(device)
 
 # Load the stored model parameters to the model instance
 checkpoint = torch.load(model_path)
 model.load_state_dict(checkpoint['state_dict'])
 
+# Trace the network
+inputs = torch.rand(1, 3, h, w).to(device)
+print(inputs)
 traced_net = torch.jit.trace(model, inputs)
+# traced_net = torch.jit.trace(model, )
+print("Trace done")
+
+# Save the module
+traced_net.save("ENet_h{}_w{}.pt".format(h,w))
+print("ENet_h{}_w{}.pt is exported".format(h,w))
