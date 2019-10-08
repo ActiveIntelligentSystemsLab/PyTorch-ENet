8341095e8b8f3185f4f47803d8ec203da3fefb18
ShigemichiMatsuzaki <matsuzaki@aisl.cs.tut.ac.jp>
2019-10-08 11:32:55 +0900

    [add] Add codes of a class to wrap operations regarding PyTorch

--- src/example-app.cpp
+++ src/example-app.cpp
@@ -2,23 +2,25 @@
 #include <opencv2/opencv.hpp>
 #include "opencv2/highgui/highgui.hpp"
 
+#include "pytorch_cpp_wrapper/pytorch_cpp_wrapper.h"
+
 #include <iostream>
 #include <memory>
 
-std::shared_ptr<torch::jit::script::Module> import_module(const char* filename)
-{
-    std::shared_ptr<torch::jit::script::Module> module;
-    try {
-        // Deserialize the ScriptModule from a file using torch::jit::load().
-        module = torch::jit::load(filename);
-    }
-    catch (const c10::Error& e) {
-        std::cerr << "error loading the model\n";
-        return nullptr;
-    }
-
-    return module;
-}
+// std::shared_ptr<torch::jit::script::Module> import_module(const char* filename)
+// {
+//     std::shared_ptr<torch::jit::script::Module> module;
+//     try {
+//         // Deserialize the ScriptModule from a file using torch::jit::load().
+//         module = torch::jit::load(filename);
+//     }
+//     catch (const c10::Error& e) {
+//         std::cerr << "error loading the model\n";
+//         return nullptr;
+//     }
+// 
+//     return module;
+// }
 
 int main(int argc, const char* argv[]) {
     // Check the number of arguments
@@ -28,37 +30,47 @@ int main(int argc, const char* argv[]) {
     }
     
     // Import trained model
-    std::shared_ptr<torch::jit::script::Module> module = import_module(argv[1]);
+    PyTorchCppWrapper pt_wrapper(argv[1]);
+    // std::shared_ptr<torch::jit::script::Module> module = import_module(argv[1]);
 
     // Import an image
     cv::Mat input_img = cv::imread(argv[2]);
 
     // Get the size of the input image
-    int height = input_img.size().height;
-    int width  = input_img.size().width;
+    // int height = input_img.size().height;
+    // int width  = input_img.size().width;
+
 
     // Create a vector of inputs.
-    at::Tensor input_tensor_ones  = torch::ones({1, 3, 264, 480}).to(torch::kCUDA);
-    std::vector<int64_t>shape = {1, height, width, 3};
-    at::Tensor input_tensor = torch::from_blob(input_img.data, at::IntList(shape), at::ScalarType::Byte).to(torch::kFloat).to(torch::kCUDA);
-    input_tensor = at::transpose(input_tensor, 1, 2);
-    input_tensor = at::transpose(input_tensor, 1, 3);
+    //at::Tensor input_tensor_ones  = torch::ones({1, 3, 264, 480}).to(torch::kCUDA);
+    // std::vector<int64_t>shape = {1, height, width, 3};
+    // at::Tensor input_tensor = torch::from_blob(input_img.data, at::IntList(shape), at::ScalarType::Byte).to(torch::kFloat).to(torch::kCUDA);
+    // input_tensor = at::transpose(input_tensor, 1, 2);
+    // input_tensor = at::transpose(input_tensor, 1, 3);
+    at::Tensor input_tensor;
+    pt_wrapper.img2tensor(input_img, input_tensor);
 
 //    std::cout << input_tensor << std::endl;
 
-    std::cout << input_tensor << std::endl;
+    std::cout << input_tensor.sizes() << std::endl;
 
     // Execute the model and turn its output into a tensor.
-    at::Tensor output = module->forward({input_tensor}).toTensor();
+//    at::Tensor output = module->forward({input_tensor}).toTensor();
+    at::Tensor output = pt_wrapper.get_output(input_tensor);
     // Calculate argmax to get a label on each pixel
-    at::Tensor output_args = at::argmax(output, 1).to(torch::kCPU).to(at::kByte);
+    // at::Tensor output_args = at::argmax(output, 1).to(torch::kCPU).to(at::kByte);
+    at::Tensor output_args = pt_wrapper.get_argmax(output);
 
     // Convert to OpenCV
-    cv::Mat mat(height, width, CV_8U, output_args[0]. template data<uint8_t>());
+    // cv::Mat mat(height, width, CV_8U, output_args[0]. template data<uint8_t>());
+    cv::Mat mat;
+    pt_wrapper.tensor2img(output_args[0], mat);
+    std::cout << output_args[0].sizes() << std::endl;
+    std::cout << mat.size() << std::endl;
 
 //    std::cout << mat.size() << std::endl;
 
 //    mat.convertTo(mat, CV_8U, 255);
     cv::imwrite("hoge.jpg", mat);
-    std::cout << mat << std::endl;
+//    std::cout << mat << std::endl;
 }
